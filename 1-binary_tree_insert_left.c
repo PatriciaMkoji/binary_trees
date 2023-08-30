785f5c596d927efaf2ba62619736867d1c9d9ccb
PatriciaMkoji <keyshotrisha@gmail.com>
2023-08-30 17:25:38 +0000

    corrected how left node updates

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return(NULL);
 
 	newnode->n = value;
-	newnode->left = NULL;
+	newnode->left = parent->left;
 	newnode->right = NULL;
 
 	if (parent->left != NULL)
